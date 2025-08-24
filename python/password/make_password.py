e53c5db3a1b3c2485a877e309f5d6b4e5eb151c5
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-24 02:51:21 -0300

    Make Password

---
+++ python/password/make_password.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+tamanho = 12
+caracteres = string.ascii_letters + string.digits + string.punctuation
+senha = "".join(random.choice(caracteres) for _ in range(tamanho))
+print("Senha gerada:", senha)
